6fb656d8dc4e3a662727aeb255d8557ff31999e9
Nicholas Paun <nicholas@transit.app>
2022-02-16 13:24:16 -0800

    This contains all components we need but not in the right order

--- blocks_to_transfers/simplify_graph.py
+++ blocks_to_transfers/simplify_graph.py
@@ -1,5 +1,5 @@
 import collections
-from posixpath import split
+import types
 from blocks_to_transfers import editor
 
 from blocks_to_transfers.editor.schema import Transfer, TransferType
@@ -121,12 +121,147 @@ def simplify(gtfs, services, generated_transfers):
     # but then what? what's the point of this graph structure: basically every transfer has 0/1 in edge and 0/1 out edge
     #   propagation is still important and becomes messier
     #   vehicle split/couple still needs detection
-    
+
     import_generated_transfers(graph, generated_transfers) 
     verify_constraints(graph)
-    backprop_split(graph)
-    export_visit(graph)
-    
+    prohibited_nodes(graph)
+    dfs_path(graph)
+    #backprop_split(graph)
+    #export_visit(graph)
+    #dfs_exp(graph)
+
+class StackCell:
+    def __init__(self, node, days, parent) -> None:
+        self.node = node
+        self.days = days
+        self.parent = parent
+
+def ppath(cell):
+    path = []
+    path.append(cell.node.trip_id)
+    while cell.parent:
+        cell = cell.parent
+        path.insert(0, cell.node.trip_id)
+
+    return path
+
+NilTrip = types.SimpleNamespace(trip_id='NIL')
+
+def prohibited_nodes(graph):
+    stack = collections.deque(graph.sources)
+    visited = set()
+    while stack:
+        node = stack.pop()
+        if node in visited:
+            continue
+
+        visited.add(node)
+
+        to_days_remaining = node.days
+        # Very similar to the block algo but also works on user-defined transfers
+        for to_node in node.out_edges.keys():
+            shift_days = -1 if to_node.trip.first_departure < node.trip.last_arrival else 0
+            to_days_in_from_ref = to_node.days.shift(shift_days)
+            to_days_remaining = to_days_remaining.difference(to_days_in_from_ref)
+            stack.append(to_node)
+
+        from_days_remaining = node.days
+        for from_node in node.in_edges.keys():
+            shift_days = 1 if node.trip.first_departure < from_node.trip.last_arrival else 0
+            from_days_in_to_ref = from_node.days.shift(shift_days)
+            from_days_remaining = from_days_remaining.difference(from_days_in_to_ref)
+        
+        if to_days_remaining:
+            print(f'NIL out of {node.trip_id} on {graph.services.bdates(to_days_remaining)}')
+            nil_node = Node(NilTrip, to_days_remaining, {node: NilTrip})
+            graph.adjust(nil_node)
+
+        if from_days_remaining:
+            print(f'NIL into {node.trip_id} on {graph.services.bdates(from_days_remaining)}')
+            nil_node = Node(NilTrip, from_days_remaining, {}, {node: NilTrip})
+            graph.adjust(nil_node)
+        graph.adjust(node)
+
+def dfs_path(graph):
+    stack = collections.deque(StackCell(node, node.days, None) for node in graph.sources)
+    print(graph.sources)
+    while stack:
+        from_node = stack.pop()
+        print(f'{ppath(from_node)}, days {graph.services.bdates(from_node.days)}')
+
+        for to_node in from_node.node.out_edges:
+            if from_node.node.trip is NilTrip or to_node.trip is NilTrip:
+                shift_days = 0
+            else:
+                shift_days = -1 if to_node.trip.first_departure < from_node.node.trip.last_arrival else 0
+
+            to_days_in_from_ref = to_node.days.shift(shift_days)
+            match_days = from_node.days.intersection(to_days_in_from_ref)
+            # undo the shift on match days
+            match_days = match_days.shift(-shift_days)
+
+            if match_days:
+                if to_node.trip is not NilTrip:
+                    stack.append(StackCell(to_node, match_days, from_node))
+                else:
+                    print(f'\033[0;32m{ppath(from_node)}, last from_days was {graph.services.bdates(from_node.days)}' \
+                        + f' to node claims to be {graph.services.bdates(to_node.days)}'
+                        + f' days {graph.services.bdates(match_days)}\033[0m')
+
+
+
+def dfs_exp(graph):  
+     stack = collections.deque(StackCell(node, node.days, None) for node in graph.sources)
+
+     while stack:
+         from_node = stack.pop()
+         #print(f'h {from_node.node.trip_id}')
+         #print(f'Block = {ppath(from_node)}, Service = {graph.services.bdates(from_node.days)}')
+
+
+         # 2. Consider all children on the current path 
+         ok = False
+         for to_node in from_node.node.out_edges:
+            shift_days = -1 if to_node.trip.first_departure < from_node.node.trip.last_arrival else 0
+            to_days_in_from_ref = to_node.days.shift(shift_days)
+            match_days = from_node.days.intersection(to_days_in_from_ref)
+            if not match_days:
+                continue
+
+            from_but_not_to = from_node.days.difference(to_days_in_from_ref)
+            to_but_not_from = to_days_in_from_ref.difference(from_node.days)
+            print(f'''{from_node.node.trip_id} -> {to_node.trip_id}. 
+            F+T: {graph.services.bdates(match_days)}
+              F: {graph.services.bdates(from_but_not_to)}
+              T: {graph.services.bdates(to_but_not_from)}
+        ''')
+
+
+            if match_days:
+                stack.append(StackCell(to_node, match_days, from_node))
+                ok = True
+            if to_but_not_from:
+                stack.append(StackCell(to_node, to_but_not_from, None))
+
+            if from_but_not_to:
+                ok = False
+
+         # END OF BLOCK, prepare to export
+         if ok:
+            continue
+
+         export_days = from_node.days
+         print(f'If {graph.services.bdates(export_days)}')
+         cell = from_node
+         while cell:
+            print(f'\t{cell.node.trip_id}')
+            cell.node.days = cell.node.days.difference(export_days)
+            cell = cell.parent
+           
+
+
+            
+
 
 def import_provided_transfers(graph):
     for transfers in graph.gtfs.transfers.values():
