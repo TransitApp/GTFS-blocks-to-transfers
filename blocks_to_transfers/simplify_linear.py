ea1621932d50ee894178f93c376e9b6132b03ea9
Rodrigo Hausen <hausen@compscinet.org>
2025-04-30 14:19:56 -0400

    break_cycles: change assert to warning
    The condition seems to be too strict and is causing GCRTA to crash.
    https://transit.slack.com/archives/C069BRR8B/p1746021400806909

--- blocks_to_transfers/simplify_linear.py
+++ blocks_to_transfers/simplify_linear.py
@@ -1,5 +1,6 @@
 import collections
 import enum
+import logging
 from . import simplify_graph
 from .service_days import ServiceDays
 from .logs import Warn
@@ -28,15 +29,19 @@ def break_cycles(graph):
 
     stack = collections.deque(graph.sources)
     state = {}
+    parents = collections.defaultdict(list)
 
     while stack:
         from_node = stack[-1]
 
-        assert state.get(from_node) != Visited.EXIT
-
-        if state.get(from_node) == Visited.ENTER:
+        node_state = state.get(from_node)
+        if node_state == Visited.ENTER:
             # All children of this node are now visited
             state[from_node] = Visited.EXIT
+            stack.pop()
+            continue
+        elif node_state == Visited.EXIT:
+            logging.warning(f'Node {from_node} has multiple parents: {parents[from_node]}')
             stack.pop()
             continue
 
@@ -44,16 +49,13 @@ def break_cycles(graph):
         state[from_node] = Visited.ENTER
 
         for to_node in list(from_node.out_edges.keys()):
-            shift_days = ServiceDays.get_shift(from_node.trip, to_node.trip)
-            match_days = from_node.days.intersection(
-                to_node.days.shift(shift_days))
-
-            if to_node not in state:
+            node_state = state.get(to_node)
+            if node_state is None:
                 # Unvisited node
+                parents[to_node].append(from_node)
                 stack.append(to_node)
                 continue
-
-            if state[to_node] == Visited.EXIT:
+            elif node_state == Visited.EXIT:
                 # Alternative path to a visited node
                 continue
 
@@ -61,6 +63,9 @@ def break_cycles(graph):
             # of from_node and source node of to_node
             graph.del_edge(from_node, to_node)
 
+            shift_days = ServiceDays.get_shift(from_node.trip, to_node.trip)
+            match_days = from_node.days.intersection(
+                to_node.days.shift(shift_days))
             match_days_reshifted = match_days.shift(-shift_days)
             from_node.sink_node.days = from_node.sink_node.days.union(
                 match_days_reshifted)
