deb3491d35d6af84d995486489c08fdbbc885fbd
Rodrigo Hausen <hausen@compscinet.org>
2025-05-01 18:23:54 -0400

    do not export env vars

--- upload.sh
+++ upload.sh
@@ -1,7 +1,7 @@
 # If you get any errors, run `pip install setuptools wheel twine`
 rm -rf dist
 python3 setup.py sdist bdist_wheel
-export TWINE_USERNAME=transit
-export TWINE_PASSWORD=`op read 'op://Shared/64ojrnriw5gmll4qtqebvtfwpe/password'`
-export TWINE_REPOSITORY_URL=https://pypi.transitapp.com:443
-twine upload dist/*
+TWINE_USERNAME=transit \
+    TWINE_PASSWORD=`op read 'op://Shared/64ojrnriw5gmll4qtqebvtfwpe/password'` \
+    TWINE_REPOSITORY_URL=https://pypi.transitapp.com:443 \
+    twine upload dist/* \
